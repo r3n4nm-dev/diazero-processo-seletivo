9771da4a399c296e7351772c7d44939385871cc1
r3n4nm-dev <r3n4nm@gmail.com>
2021-07-28 20:21:55 -0300

    Project created

---
+++ src/main/java/com/renanmateus/diazero/DiazeroProcessoSeletivoApplication.java
@@ -0,0 +1,13 @@
+package com.renanmateus.diazero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiazeroProcessoSeletivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiazeroProcessoSeletivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renanmateus/diazero/DiazeroProcessoSeletivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanmateus.diazero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiazeroProcessoSeletivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
